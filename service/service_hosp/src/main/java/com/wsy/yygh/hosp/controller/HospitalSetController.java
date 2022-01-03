9c64e8ccaabdb1c208bd9cdfa6761ce59cce76d8
王世寅 <1026844615@qq.com>
2022-01-03 17:07:48 +0800

    3

--- service/service_hosp/src/main/java/com/wsy/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/wsy/yygh/hosp/controller/HospitalSetController.java
@@ -1,11 +1,16 @@
 package com.wsy.yygh.hosp.controller;
 
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.atguigu.yygh.vo.hosp.HospitalSetQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wsy.yygh.common.result.Result;
 import com.wsy.yygh.hosp.service.HospitalSetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Wrapper;
 import java.util.List;
 
 @RestController
@@ -30,5 +35,27 @@ public class HospitalSetController {
             return Result.ok();
         }
         return Result.fail();
+    }
+    //3.条件查询
+    @PostMapping ("findPage/{current}/{limit}")
+    public Result findPageHospSet(@PathVariable long current,
+                                  @PathVariable long limit,
+                                  @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo){
+
+        Page<HospitalSet> page = new Page<>(current,limit);
+        QueryWrapper<HospitalSet> Wrapper = new QueryWrapper<>();
+
+        String hosname = hospitalSetQueryVo.getHosname();
+        String hoscode = hospitalSetQueryVo.getHoscode();
+
+        if (!StringUtils.isEmpty(hosname)){
+            Wrapper.like("hosname",hospitalSetQueryVo.getHosname());
+        }
+        if (!StringUtils.isEmpty(hoscode)){
+            Wrapper.eq("hoscode",hospitalSetQueryVo.getHoscode());
+        }
+
+        Page<HospitalSet> page1 = hospitalSetService.page(page, Wrapper);
+        return Result.ok(page1);
     }
 }
